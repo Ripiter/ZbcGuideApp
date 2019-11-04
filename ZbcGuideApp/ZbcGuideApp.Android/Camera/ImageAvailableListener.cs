579373f666e0c47727e4d284f1085163ec89d167
Marc Andersen <marchot1999@gmail.com>
2019-11-04 10:55:41 +0100

    Camera merged to main program

---
+++ ZbcGuideApp/ZbcGuideApp.Android/Camera/ImageAvailableListener.cs
@@ -0,0 +1,34 @@
+﻿using Android.Media;
+using System;
+
+namespace ZbcGuideApp.Droid.Camera
+{
+    class ImageAvailableListener : Java.Lang.Object, ImageReader.IOnImageAvailableListener
+    {
+        public event EventHandler<byte[]> Photo;
+
+        public void OnImageAvailable(ImageReader reader)
+        {
+            Image image = null;
+
+            try
+            {
+                image = reader.AcquireLatestImage();
+                var buffer = image.GetPlanes()[0].Buffer;
+                var imageData = new byte[buffer.Capacity()];
+                buffer.Get(imageData);
+
+                Photo?.Invoke(this, imageData);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+            finally
+            {
+                image?.Close();
+            }
+        }
+    }
+
+}
