de5b1eefc8e33735afed10b4dceeccf842f7a64f
Peter <ripiter15@wp.pl>
2019-11-21 13:30:49 +0100

    Fixed Names

---
+++ ZbcGuideApp/ZbcGuideApp/KnownAccessPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZbcGuideApp
+{
+    class KnownAccessPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Strenght { get; set; }
+    }
+}
