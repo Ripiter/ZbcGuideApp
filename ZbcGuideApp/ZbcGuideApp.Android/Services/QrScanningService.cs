491653a6999d0e8651ca870f0dd8d9e153a724cf
PeterLothsen <lothsen89@hotmail.com>
2020-01-09 15:50:12 +0100

    Add Camera, QRkode

---
+++ ZbcGuideApp/ZbcGuideApp.Android/Services/QrScanningService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ZbcGuideApp.Services;
+using ZXing.Mobile;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ZbcGuideApp.Droid.Services.QrScanningService))]
+
+namespace ZbcGuideApp.Droid.Services
+{
+    public class QrScanningService : IQrScanningService
+    {
+        public async Task<string> ScanAsync()
+        {
+            var optionsDefault = new MobileBarcodeScanningOptions();
+            var optionsCustom = new MobileBarcodeScanningOptions();
+
+            var scanner = new MobileBarcodeScanner()
+            {
+                TopText = "Scan the QR Code",
+                BottomText = "Please Wait",
+            };
+
+            var scanResult = await scanner.Scan(optionsCustom);
+            return scanResult.Text;
+        }
+    }
+}
---
+++ ZbcGuideApp/ZbcGuideApp/QrCodeMain.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZbcGuideApp.Services;
+
+namespace ZbcGuideApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QrCodeMain : ContentPage
+    {
+        public QrCodeMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnScan_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var scanner = DependencyService.Get<IQrScanningService>();
+                var result = await scanner.ScanAsync();
+                if (result != null)
+                {
+                    txtBarcode.Text = result;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
